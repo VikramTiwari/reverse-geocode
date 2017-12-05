e2b554c5cf55eaed70284c3419d5ce025d4562b7
James Perih <jperih@gasbuddy.com>
2017-12-05 10:56:43 -0600

    Separate US lookups

--- test/reverse-geocode.js
+++ test/reverse-geocode.js
@@ -3,28 +3,28 @@
 let assert = require('assert'),
   reverse = require('../lib/reverse-geocode');
 
-let city = reverse.lookup(37.8072792, -122.4780652);
+let usa = reverse.lookup(37.8072792, -122.4780652);
 
 describe('lookup()', function() {
   it('zipcode', function() {
-    assert.equal(94129, city.zipcode);
+    assert.equal(94129, usa.zipcode);
   });
   it('state_abbr', function() {
-    assert.equal('CA', city.state_abbr);
+    assert.equal('CA', usa.state_abbr);
   });
   it('latitude', function() {
-    assert.equal('37.799840', city.latitude);
+    assert.equal('37.799840', usa.latitude);
   });
   it('longitude', function() {
-    assert.equal('-122.46167', city.longitude);
+    assert.equal('-122.46167', usa.longitude);
   });
   it('city', function() {
-    assert.equal('San Francisco', city.city);
+    assert.equal('San Francisco', usa.city);
   });
   it('state', function() {
-    assert.equal('California', city.state);
+    assert.equal('California', usa.state);
   });
   it('distance', function() {
-    assert.equal(1.6610566475026183, city.distance);
+    assert.equal(1.6610566475026183, usa.distance);
   });
 });
