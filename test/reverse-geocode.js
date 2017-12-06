d5489bd1345e5ed5f46b4b078032189f8b000cfe
James Robert Perih <james@hotdang.ca>
2017-12-05 21:02:29 -0600

    Support any mapping file.

--- test/reverse-geocode.js
+++ test/reverse-geocode.js
@@ -4,8 +4,33 @@ let assert = require('assert'),
   reverse = require('../lib/reverse-geocode');
 
 describe('lookup()', function() {
+  describe('USA', function() {
+    let usa = reverse.lookup(37.8072792, -122.4780652, 'us');
+    it('zipcode', function() {
+      assert.equal(94129, usa.zipcode);
+    });
+    it('state_abbr', function() {
+      assert.equal('CA', usa.state_abbr);
+    });
+    it('latitude', function() {
+      assert.equal('37.799840', usa.latitude);
+    });
+    it('longitude', function() {
+      assert.equal('-122.46167', usa.longitude);
+    });
+    it('city', function() {
+      assert.equal('San Francisco', usa.city);
+    });
+    it('state', function() {
+      assert.equal('California', usa.state);
+    });
+    it('distance', function() {
+      assert.equal(1.6610566475026183, usa.distance);
+    });
+  });
+
   describe('Canada', function() {
-    let ca = reverse.lookup(50.2343, -110.3438, reverse.COUNTRIES.CANADA);
+    let ca = reverse.lookup(50.2343, -110.3438, 'ca');
 
     it('postalCode', function() {
       assert.equal('T1A', ca.zipcode);
@@ -30,56 +55,29 @@ describe('lookup()', function() {
     });
   });
 
-  describe('USA', function() {
-    let usa = reverse.lookup(37.8072792, -122.4780652, reverse.COUNTRIES.USA);
+  describe('Australia', function() {
+    let au = reverse.lookup(-34.988328, 138.515406, 'au');
+
     it('zipcode', function() {
-      assert.equal(94129, usa.zipcode);
+      assert.equal(5045, au.zipcode);
     });
     it('state_abbr', function() {
-      assert.equal('CA', usa.state_abbr);
+      assert.equal('SA', au.state_abbr);
     });
     it('latitude', function() {
-      assert.equal('37.799840', usa.latitude);
+      assert.equal('-34.9833', au.latitude);
     });
     it('longitude', function() {
-      assert.equal('-122.46167', usa.longitude);
+      assert.equal(138.5167, au.longitude);
     });
     it('city', function() {
-      assert.equal('San Francisco', usa.city);
+      assert.equal('Glenelg Jetty Road', au.city);
     });
     it('state', function() {
-      assert.equal('California', usa.state);
+      assert.equal('South Australia', au.state);
     });
     it('distance', function() {
-      assert.equal(1.6610566475026183, usa.distance);
-    });
-  });
-
-  describe('Dynamic file(s)', function() {
-    describe('Australia', function() {
-      let au = reverse.lookup(-34.988328, 138.515406, 'au');
-
-      it('zipcode', function() {
-        assert.equal(5045, au.zipcode);
-      });
-      it('state_abbr', function() {
-        assert.equal('SA', au.state_abbr);
-      });
-      it('latitude', function() {
-        assert.equal('-34.9833', au.latitude);
-      });
-      it('longitude', function() {
-        assert.equal(138.5167, au.longitude);
-      });
-      it('city', function() {
-        assert.equal('Glenelg Jetty Road', au.city);
-      });
-      it('state', function() {
-        assert.equal('South Australia', au.state);
-      });
-      it('distance', function() {
-        assert.equal(0.5713811323631017, au.distance);
-      });
+      assert.equal(0.5713811323631017, au.distance);
     });
   });
 
