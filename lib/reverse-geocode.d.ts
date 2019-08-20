e4f5cad13f2470b80a0cdc52ed5c1c6ee5117fbf
Peter Kazazes <peter@peterk.co>
2019-08-20 12:31:20 -0700

    add typings

---
+++ lib/reverse-geocode.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for reverse-geocode
+// Definitions by: Peter Kazazes <https://peterk.co>
+
+export function lookup(
+  latitude: number,
+  longitude: number,
+  countryCode: string
+): ReverseGeocode;
+
+export interface ReverseGeocode {
+    country?:   string;
+    zipcode:    number | string;
+    city?:      string;
+    state:      string;
+    state_abbr: string;
+    region?:    string;
+    latitude:   number;
+    longitude:  number;
+}
